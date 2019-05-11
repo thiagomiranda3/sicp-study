b186b0f7b7329ca62032687ecf578b5dd3f478c7
Thiago Miranda <marine.rlivielor@gmail.com>
2019-05-11 03:05:50 +0000

    até exercício 1.3

--- Cap 1.rkt
+++ Cap 1.rkt
@@ -6,14 +6,18 @@
 ; Cria a constante idade com o valor 24
 (define idade 24)
 
-; compound procedures
-(define (square x) (* x x))
-
 ; cond é o if do scheme. Cada operando é uma condição que recebe um predicado no primeiro parâmetro e a função no segundo
 (define (abs x)
   (cond ((< x 0) (- x))
         (else x)))
 
+; Exercício 1.2
+(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5))))) (* 3 (- 6 2) (- 2 7)))
+
+; Exercício 1.3
 (define (square x) (* x x))
 
-(define (sum_larger x y z))
+(define (sum-square x y z)
+  (cond ((and (>= x z) (>= y z)) (+ (square x) (square y)))
+        ((and (>= x y) (>= z y)) (+ (square x) (square z)))
+        ((and (>= y x) (>= z x)) (+ (square y) (square z)))))
